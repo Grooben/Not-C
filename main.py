885509463d344ab4f3d0c369c90f3925464eb3d8
Tomasz Zajac <tomezpl@gmail.com>
2019-03-10 14:49:43 +0000

    Replaces execfile()

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 #Author : Craig Clephane
 
-execfile("LexicalAnalysis.py")
+import LexicalAnalysis as lex
 
 #Reads source file
-file = open("sourceFile.txt", "r")
+lex.file = open("sourceFile.txt", "r")
 
 #Loop which 
 while True:
-    tokenStream  =  getToken()         ##GRABS TOKEN STREAM 
+    tokenStream  =  lex.getToken()         ##GRABS TOKEN STREAM 
     token        =  tokenStream[0]     ##GRABS TOKEN
     Line         =  tokenStream[1]     ##GRABS LINE OF TOKEN
     Column       =  tokenStream[2]     ##GRABS COLUMN OF TOKEN 
 
     #Prints token visually, comment over if needed.
-    print("%5d  %5d   %-14s" % (Line, Column, all_syms[token]), end='')     
+    print("%5d  %5d   %-14s" % (Line, Column, lex.tokentable.all_syms[token]), end='')     
 
-    if token == TokenInteger: print("  %5d" % (tokenStream[3]))
-    elif token == TokenString: print(' "%s"' % (tokenStream[3]))
-    elif token == TokenIdent: print("  %s" % (tokenStream[3]))
+    if token == lex.tokentable.TokenInteger: print("  %5d" % (tokenStream[3]))
+    elif token == lex.tokentable.TokenString: print(' "%s"' % (tokenStream[3]))
+    elif token == lex.tokentable.TokenIdent: print("  %s" % (tokenStream[3]))
     else: print("")
 
     #Ends loop if 'TokenEOF' is detected. 
-    if token == TokenEOF:
+    if token == lex.tokentable.TokenEOF:
         break
