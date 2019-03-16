5465d48959c551d2e2100952c3a37126f8e2d067
MacaScull <35259402+MacaScull@users.noreply.github.com>
2019-03-16 03:21:55 +0000

    Comment update

--- semantics.py
+++ semantics.py
@@ -1,7 +1,12 @@
+# Author : Macauley Scullion
 # Interpreter version
-#test
+# will need editing into representing intermediate code and after collaboration with syntax analysis
+
+ #Import symtable functions
 import symtable
 
+##
+# Class defintion - will be removed, require by syntax and functions can simply be imported and called
 class Node: 
     def __init__(self, nodetype, char, lhn = None, rhn = None):
         self.nodetype = nodetype
@@ -9,48 +14,57 @@ class Node:
         self.lhn = lhn
         self.rhn = rhn
 
-    def eval(node):
-        switcher = {
-            "equal": node.equal,
-            "addition": node.add,
-            "subtraction": node.sub,
-            "multiply": node.mult,
-            "divide": node.div,
-            "variable": node.var,
-            "constant": node.const
-            }
-        function = switcher[node.nodetype]
-        return function()
 
-    def equal(node):
-        r = node.rhn.eval()
-        l = symtable.lookup(node.lhn.char)
-        symtable.set_attribute(l, r)
-        return
+# Node evaluation function - will evaluate the root node or passed node through a swither and call function
+def eval(node):
+    switcher = {
+        "equal": node.equal,
+        "addition": node.add,
+        "subtraction": node.sub,
+        "multiply": node.mult,
+        "divide": node.div,
+        "variable": node.var,
+        "constant": node.const
+        }
+    function = switcher[node.nodetype]
+    return function()
 
-    def add(node):
-        l = node.lhn.eval()
-        r = node.rhn.eval()
-        return l + r
+# Equal function - evals right side, assigns left return value from right eval
+def equal(node):
+    r = node.rhn.eval()
+    l = symtable.lookup(node.lhn.char)
+    symtable.set_attribute(l, r)
+    return
 
-    def sub(node):
-        l = node.lhn.eval()
-        r = node.rhn.eval()
-        return l - r
+# Add function - returns left and right addition
+def add(node):
+    l = node.lhn.eval()
+    r = node.rhn.eval()
+    return l + r
 
-    def mult(node):
-        l = node.lhn.eval()
-        r = node.rhn.eval()
-        return l * r
+# Sub function - returns left and right subtraction 
+def sub(node):
+    l = node.lhn.eval()
+    r = node.rhn.eval()
+    return l - r
 
-    def div(node):
-        l = node.lhn.eval()
-        r = node.rhn.eval()
-        return l / r
+# mult function - returns left and right multiplication 
+def mult(node):
+    l = node.lhn.eval()
+    r = node.rhn.eval()
+    return l * r
 
-    def var(node):
-        var = symtable.lookup(node.char)
-        return var.value
+# div function - returns left and right division
+def div(node):
+    l = node.lhn.eval()
+    r = node.rhn.eval()
+    return l / r
 
-    def const(node):
-        return node.char
+# var function - returns variable value after lookup 
+def var(node):
+    var = symtable.lookup(node.char)
+    return var.value
+
+# const function - returns the constant i.e. its name
+def const(node):
+    return node.char
