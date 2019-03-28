6662f5a2664df7647665b3858bb6661f5969267f
Oliver Grooby <olliegrooby@gmail.com>
2019-03-28 12:03:13 +0000

    Began remake of optimiser
    Push every time god damn

--- optimiser.py
+++ optimiser.py
@@ -1,7 +1,17 @@
 # Optimiser v2 - electric bugaloo
 # Author: Oliver Grooby
 
+import LexicalAnalysis as lex
+
 # Check for redundant Assignments
 def remove_redundant_assign():
     for node in tree: # Tree doesn't exist
-        
+        if node.token[0].type == Oassign:
+            if node.token[0].value == sym.value:
+                # Logic to remove tokens
+
+# Check for if statements that always eval as true
+def remove_always_true():
+    for node in tree:
+        if node.token[0].type == KeywordIF:
+            # Insert Logic to detect a If statement that always evaluates as true
