0d750b0feb9ec5629f11f75a45184cecd0eb283f
Oliver Grooby <olliegrooby@gmail.com>
2019-03-28 12:17:39 +0000

    Awaiting completed parse tree for optimisation

--- optimiser.py
+++ optimiser.py
@@ -2,16 +2,27 @@
 # Author: Oliver Grooby
 
 import LexicalAnalysis as lex
+import parseTreeGeneration as pT
+
+def remove_node(node):
+    print("Will Remove Node...")
 
 # Check for redundant Assignments
 def remove_redundant_assign():
+
     for node in tree: # Tree doesn't exist
         if node.token[0].type == Oassign:
             if node.token[0].value == sym.value:
-                # Logic to remove tokens
+                remove_node(node)
+                
+
+# Lol most of this isn't needed now
 
 # Check for if statements that always eval as true
+
 def remove_always_true():
+
     for node in tree:
         if node.token[0].type == KeywordIF:
             # Insert Logic to detect a If statement that always evaluates as true
+            remove_node(node)
