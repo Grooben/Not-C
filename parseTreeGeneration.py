11c47ff9272e174ff02f80665512238ec864159f
Peter Deskptop <Peter.P.Maltby@gmail.com>
2019-03-30 16:07:01 +0000

    chnage over to recursive tree gen, fixing error and trying to get the thing to work.

--- parseTreeGeneration.py
+++ parseTreeGeneration.py
@@ -5,21 +5,21 @@ from semantics import Node
 
 class Buffer:  ##Buffer class contains all manipulation code.
     data=[]
-    i =0
+    count = 0
 
     def __init__(self):
         data = []
 
-    def find(self, criteria, d = [], i = 0):
-        for Node in d:
+    def find(self, criteria, i = 0):
+        for Node in self.data:
             if Node.catagory == criteria: return i
             i +=1
         return None
 
-    def find(self, criteria, d = [], i = 0,max= 1000) :##todo remove hardcoded max
-        for Node in d:
+    def find(self, criteria, i = 0,max = 1000) :##todo remove hardcoded max
+        for Node in self.data:
             if Node.catagory == criteria: return i
-            if i<max: return None
+            if i>max: return None
             i +=1  
         return None
     
@@ -28,20 +28,18 @@ class Buffer:  ##Buffer class contains all manipulation code.
         root = None
         splice = None # temp vlaue for storing spice postion for recurision.
         
-        print((self.find ("Assignment",self.data)))
-        if (self.find ("Assignment",self.data)):
-            print("assignement ma boi");
-            if self.find("Assignment",self.data,self.find("Assignment",self.data)):##ensures only one assign per line.
-                Print("Error Multiple assign") #add error handeling
-            root= self.data[self.find ("Assignment",self.data)]
-            splice =  self.data[self.find ("Assignment",self.data)]
-        elif (self.find("Function",self.data)):
-            root= self.data[self.find ("Function",self.data)]
-            root= self.data[self.find ("Function",self.data)]
-            splice =  self.data[self.find ("Function",self.data)]
+        print((self.find ("Assignment")))
+        if (self.find ("Assignment")):
+            #if self.find("Assignment",self.find("Assignment")+1):##ensures only one assign per line.
+                #print("Error Multiple assign") #add error handeling
+            root= self.data[self.find ("Assignment")]
+            splice =  self.find ("Assignment")
+        elif (self.find("Function")):
+            root= self.data[self.find ("Function")]
+            splice =  self.find ("Function")
         if splice != None:
             if splice>1: self.Eval(root,True,0,splice-1)
-            if splice<len(data)-1: Eval(root,False,splice+1)
+            if splice<len(self.data)-1: self.Eval(root,False,splice+1)
 
 
 
@@ -49,33 +47,33 @@ class Buffer:  ##Buffer class contains all manipulation code.
 
     def Eval(self, parent, lhn, min=0, max=len(data)): ##recursive eval function
         print(parent, lhn, min,max)
-        if (self.find("Function",self.data,min,max)):
-            if lhn: parent.lhn= self.data[self.find ("Function",self.data,min,max)]
-            else: parent.rhn= self.data[self.find ("Function",self.data,min,max)]
-            splice =  self.data[self.find ("Function",self.data,min,max)]
-        elif (self.find("Operator",self.data,min,max)):
-            if lhn: parent.lhn= self.data[self.find ("Operator",self.data,min,max)]
-            else: parent.rhn= self.data[self.find ("Operator",self.data,min,max)]
-            splice =  self.data[self.find ("Operator",self.data,min,max)]
-        elif (self.find("Variable",self.data,min,max)):
-            if lhn: parent.lhn= self.data[self.find ("Variable",self.data,min,max)]
-            else: parent.rhn= self.data[self.find ("Variable",self.data,min,max)]
+        if (self.find("Function",min,max)):
+            if lhn: parent.lhn= self.data[self.find ("Function",min,max)]
+            else: parent.rhn= self.data[self.find ("Function",min,max)]
+            splice =  self.find ("Function",min,max)
+        elif (self.find("Operator",min,max)):
+            if lhn: parent.lhn= self.data[self.find ("Operator",min,max)]
+            else: parent.rhn= self.data[self.find ("Operator",min,max)]
+            splice =  self.find ("Operator",min,max)
+        elif (self.find("Variable",min,max)):
+            if lhn: parent.lhn= self.data[self.find ("Variable",min,max)]
+            else: parent.rhn= self.data[self.find ("Variable",min,max)]
             return
         else : return
 
         if lhn: parent = parent.lhn##rebases parent in correct node for send 
         else: parent = parent.rhn
 
-        Eval(parent,True,min,splice-1)
-        Eval(parent,False,splice+1,max)
+        self.Eval(parent,True,min,splice-1)
+        self.Eval(parent,False,splice+1,max)
 
     def add(self, catagory, tokenType, val = None):
         print(catagory)
         self.data.append(Node(catagory, tokenType, val))
         if catagory == "StatementTerminator": 
             GeneratedTrees.append(self.StartEval())
-            self.data[self.i].PrintTree()
-            self.i =+ 1
+            self.data[self.count].PrintTree()
+            self.count =+ 1
             print("tree complete")
 
 GeneratedTrees = []
