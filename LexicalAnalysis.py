3dd2b90bf03e727e6493c2ade26d115151fe5fee
Craig <Craig-Clephane@hotmail.co.uk>
2019-04-05 19:51:05 +0100

    Blank space bug fix

--- LexicalAnalysis.py
+++ LexicalAnalysis.py
@@ -22,16 +22,31 @@ Idname = ""
 #Array containing tokens, as well as number of tokens
 TokenArray = []
 tokenCount = 0
+commentGone = False
 
 #Grabs next character from the file, as well as shifting columns (Character) along everytime this function is called.
 #Once a new line is detected, the column (Character) will reset to the start of the line, and shift line.
 def grabNextCharacter():
 
-    global Character, Column, Line, endOfLine 
+    global Character, Column, Line, endOfLine, commentGone
 
     Character = file.read(1)      
-    Column += 1       
-    if Character == '\n':   
+    Column += 1    
+
+    if (Character == '\n' and commentGone == True):
+        Line += 1
+        Column = 0
+        endOfLine = False
+        print("hello")
+        commentGone = False
+        return 
+
+    if (Character == '\n' and Column == 1):
+        Line += 1
+        Column = 0
+        return 
+
+    if (Character == '\n' and Column != 1):  
       Line += 1 
       Column = 0 
       endOfLine = True 
@@ -130,6 +145,8 @@ def identifiersOrIntegers(errLine, errCol):
 #Function which identifies whether the string is a comment as well as return the divide token.
 def commentsAndDiv(errLine, errCol):
 
+    global commentGone
+
     #If the character after '/' does not equal to a *, return token divide. 
     if grabNextCharacter() != '*':
         bufferTokens(tokentable.TokenDivide)
@@ -142,6 +159,7 @@ def commentsAndDiv(errLine, errCol):
         if Character == '*':
             if grabNextCharacter() == '/':  
                 grabNextCharacter()
+                commentGone = True
                 return getToken()
 
         elif len(Character) == 0:
